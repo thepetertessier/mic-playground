776b896e9e8c496981c2aa077ec6c954a4518518
Peter Tessier <fpv5gr@virginia.edu>
2025-02-07 23:33:26 -0500

    smooth volume

--- scripts/live_feedback.gd
+++ scripts/live_feedback.gd
@@ -11,6 +11,9 @@ var record_bus_index: int
 @export var max_db := 24.0    # Maximum gain at 100% slider
 
 var slope: float
+const BUFFER_SIZE = 10
+var sample_buffer = []
+var avg_volume: float = 0.0
 
 func _ready() -> void:
 	record_bus_index = AudioServer.get_bus_index("Record")
@@ -21,8 +24,15 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var sample = AudioServer.get_bus_peak_volume_left_db(record_bus_index, 0)
 	var linear_sample = db_to_linear(sample) * 100
+	sample_buffer.push_back(linear_sample)
 	
-	volume_bar.value = linear_sample
+	if sample_buffer.size() > BUFFER_SIZE:
+		var to_add: float = linear_sample
+		to_add -= sample_buffer.pop_front()
+		to_add /= BUFFER_SIZE
+		avg_volume += to_add
+		
+	volume_bar.value = avg_volume
 
 func _on_gain_slider_drag_ended(value_changed: bool) -> void:
 	var gain_db = (linear_to_db(gain_slider.value/100)+5)*6
